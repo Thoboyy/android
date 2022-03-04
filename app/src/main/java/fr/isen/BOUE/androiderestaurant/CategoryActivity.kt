e1279b2a6d7436bcd02f8effc19026ac45bf0ecc
thomas.boue <thobou83@gmail.Com>
2022-03-04 16:27:53 +0100

    4eme commit

--- app/src/main/java/fr/isen/BOUE/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/BOUE/androiderestaurant/CategoryActivity.kt
@@ -1,17 +1,41 @@
 package fr.isen.BOUE.androiderestaurant
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Button
-import android.content.Intent
-import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import fr.isen.BOUE.androiderestaurant.databinding.ActivityCategoryBinding
 
 class CategoryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCategoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
-        val textview = findViewById<TextView>(R.id.menu)
-        textview.text = intent.getStringExtra("Category")
+        binding = ActivityCategoryBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val array_starters = resources.getStringArray(R.array.starters_array)
+        val array_dishes = resources.getStringArray(R.array.dishes_array)
+        val array_deserts = resources.getStringArray(R.array.deserts_array)
+        binding.dishList.text = intent.getStringExtra("Category")
+        when (binding.dishList.text) {
+            "Entrees" -> print(array_starters)
+            "Plats" -> print(array_dishes)
+            "Desserts" -> print(array_deserts)
+            else -> { // Note the block
+                print("non")
+            }
+        }
+
+        // this creates a vertical layout Manager
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<String>()
+
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CategoryAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        binding.recyclerview.adapter = adapter
     }
 }
